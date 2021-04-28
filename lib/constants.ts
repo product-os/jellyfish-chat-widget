e50b85cfca1d2f8cc5568043eb08b3ca42c7311c
balena-ci <balena-ci@users.noreply.github.com>
2021-04-28 03:07:09 +0000

    deploy: c2131a8c809d2407343fbd0f42f22469d30a50e3

--- lib/constants.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
-export const INITIAL_FETCH_CONVERSATIONS_LIMIT = 15;
-export const FETCH_MORE_CONVERSATIONS_LIMIT = 30;
-export const FETCH_MORE_MESSAGES_LIMIT = 20;
